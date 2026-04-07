1b883f0b6c6e631221cdf7c38f2ee7b6f972c556
Ajaygithubproject <145328880+Ajaygithubproject@users.noreply.github.com>
2026-04-07 14:08:55 +0530

    first commit

--- immigratecrm/settings.py
+++ immigratecrm/settings.py
@@ -66,7 +66,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520
+FILE_UPLOAD_MAX_MEMORY_SIZE = 20971520
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
