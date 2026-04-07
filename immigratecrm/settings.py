0281e34c325023fb90d2be5f2f3bbf01d089105b
Ajaygithubproject <145328880+Ajaygithubproject@users.noreply.github.com>
2026-04-07 15:01:42 +0530

    production settings

--- immigratecrm/settings.py
+++ immigratecrm/settings.py
@@ -5,9 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-immigratecrm-secret-key-change-in-production-2024'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.onrender.com']
+
 
 INSTALLED_APPS = [
     'jazzmin',
@@ -17,11 +18,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'crm',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -35,14 +40,21 @@ ROOT_URLCONF = 'immigratecrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+
+        'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
+
                 'django.contrib.messages.context_processors.messages',
+
                 'django.template.context_processors.media',
             ],
         },
@@ -51,35 +63,62 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'immigratecrm.wsgi.application'
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+
 AUTH_PASSWORD_VALIDATORS = []
 
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'Asia/Kolkata'
+
 USE_I18N = True
+
 USE_TZ = True
 
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520
-FILE_UPLOAD_MAX_MEMORY_SIZE = 20971520
+# STATIC FILES CONFIGURATION
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+# MEDIA FILES CONFIGURATION
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# FILE UPLOAD LIMIT
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520
+
+FILE_UPLOAD_MAX_MEMORY_SIZE = 20971520
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# LOGIN SETTINGS
+
 LOGIN_URL = '/login/'
+
 LOGIN_REDIRECT_URL = '/'
+
 LOGOUT_REDIRECT_URL = '/login/'
